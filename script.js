7f5b0e4f3e9cbaee9ba6d04231f9c8d935b54154
Rolan <rolan2001@bk.ru>
2024-05-17 11:42:49 +0300

    form is working

---
+++ script.js
@@ -0,0 +1,15 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxX0BUes0RSd374cDHBdK6ZS_QYp4nuDtJCR4UIeQQEOgPt1OqKWERe_M2EmHXphEU/exec'
+			const form = document.forms['submit-to-google-sheet']
+			
+			console.log(new Date())
+		
+			form.addEventListener('submit', e => {
+			
+				document.querySelector('#dateForm').value = new Date()
+
+			  e.preventDefault()
+			  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+				.then(response => alert("Форма отправлена"))
+				.then(() => {  window.location.reload()})
+				.catch(error => console.error('Ошибка отправки', error.message))
+			})
